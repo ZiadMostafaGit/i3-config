7e13eb61364ca4894ca183961b239414f784021c
ziad_mostafa <ziadmostafa760@gmail.com>
2025-02-24 15:37:25 +0200

    init this config

---
+++ fish/config.fish
@@ -0,0 +1,28 @@
+set -g fish_greeting
+
+if status is-interactive
+    starship init fish | source
+end
+
+
+function fzf_cd
+    # Search for directories starting from ~/
+    set -l dir (find ~/ -type d 2>/dev/null | fzf --height 40% --reverse --prompt="Select directory: ")
+
+    # If a directory is selected, cd into it
+    if test -n "$dir"
+        cd "$dir"
+    end
+end
+
+
+bind \cd fzf_cd
+
+bind \cs search_files
+
+function search_files
+    set file (find . -type f | fzf)
+    if test -n "$file"
+        nvim "$file"
+    end
+end
